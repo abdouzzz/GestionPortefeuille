0eebcc3e48ed0efbf0b77f2a90dbbd5d10b36830
PC-ABDOU\abdou <abdououardi3@gmail.com>
2025-04-02 10:50:40 +0200

    eeee

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,21 +9,5 @@ void main() {
 
     // Construire l'interface utilisateur
     await tester.pumpWidget(const MyAppView());
-
-    // Vérifier que le compteur commence à 0
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Vérifier que le bouton d'incrémentation existe
-    final Finder incrementButton = find.byIcon(Icons.add);
-    expect(incrementButton, findsOneWidget);
-
-    // Simuler un tap sur le bouton "+"
-    await tester.tap(incrementButton);
-    await tester.pumpAndSettle();
-
-    // Vérifier que le compteur est passé à 1
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
 }
