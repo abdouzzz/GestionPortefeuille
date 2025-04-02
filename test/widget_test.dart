056850b87d9e860be5c872c8f7ad498cd3296e6c
PC-ABDOU\abdou <abdououardi3@gmail.com>
2025-04-02 10:48:59 +0200

    uydfyfdd

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,29 +1,28 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:gestion_portefeuille/main.dart';
+import 'package:gestion_portefeuille/app_view.dart'; // On teste directement MyAppView
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('Counter increments test', (WidgetTester tester) async {
+    // Assurer l'initialisation de Flutter
+    WidgetsFlutterBinding.ensureInitialized();
 
-    // Verify that our counter starts at 0.
+    // Construire l'interface utilisateur
+    await tester.pumpWidget(const MyAppView());
+
+    // Vérifier que le compteur commence à 0
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // Vérifier que le bouton d'incrémentation existe
+    final Finder incrementButton = find.byIcon(Icons.add);
+    expect(incrementButton, findsOneWidget);
 
-    // Verify that our counter has incremented.
+    // Simuler un tap sur le bouton "+"
+    await tester.tap(incrementButton);
+    await tester.pumpAndSettle();
+
+    // Vérifier que le compteur est passé à 1
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
