58625b2adb73f3d186492ca77816eded82bc767b
PC-ABDOU\abdou <abdououardi3@gmail.com>
2025-04-02 10:54:46 +0200

    sdddd

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:gestion_portefeuille/app_view.dart'; // On teste directement MyAppView
+import 'package:gestion_portefeuille/app_view.dart';
+import 'firebase_test_mock.dart'; // Importe ton mock Firebase
 
 void main() {
   testWidgets('Counter increments test', (WidgetTester tester) async {
-    // Assurer l'initialisation de Flutter
-    WidgetsFlutterBinding.ensureInitialized();
+    // Initialiser Firebase avec le mock
+    await setupFirebaseForTests();
 
     // Construire l'interface utilisateur
     await tester.pumpWidget(const MyAppView());
+
+    // Vérifier que le compteur commence à 0
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    // Vérifier que le bouton d'incrémentation existe
+    final Finder incrementButton = find.byIcon(Icons.add);
+    expect(incrementButton, findsOneWidget);
+
+    // Simuler un tap sur le bouton "+"
+    await tester.tap(incrementButton);
+    await tester.pumpAndSettle();
+
+    // Vérifier que le compteur est passé à 1
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
   });
 }
